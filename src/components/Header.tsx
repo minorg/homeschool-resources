209d417e475aab3f03efdafd385e8e4548ef0609
Minor Gordon <minorg@server.fake>
2025-08-10 15:03:19 -0400

    add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+
+function Header() {
+  return (
+    <header className="border-b">
+      <div className="container mx-auto px-6 py-2 flex justify-between items-center">
+        <a href="/" className="text-2xl font-bold tracking-tight">
+          Homeschool Hub
+        </a>
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <a href="/">Home</a>
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <a href="/resources-for-parents">For Parents</a>
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <a href="/resources-for-kids">For Kids</a>
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <a href="/contact">Contact</a>
+            </MenubarTrigger>
+          </MenubarMenu>
+        </Menubar>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
